da881ed15fbd2947ad0d859ddabce17f6bc0e313
Sam Nastase <sam.nastase@gmail.com>
2019-05-23 18:48:27 -0400

    Improved template for managing fns/metadata

--- aggregate_inputs.py
+++ aggregate_inputs.py
@@ -1,17 +1,29 @@
 import json
 
-input_fns = {'black':
-                 {'model': 'transcripts/black_word2vec_embeddings.npy',
-                  'model_trims': [0, 0],
-                  'data': {'sub-02': 'data/sub-02_task-black.fsaverage6.lh.tproject.gii',
-                           'sub-15': 'data/sub-15_task-black.fsaverage6.lh.tproject.gii'},
-                  'data_trims': [8, 8]},
-             'forgot':
-                  {'model': 'transcripts/forgot_word2vec_embeddings.npy',
-                   'model_trims': [0, 0],
-                   'data': {'sub-02': 'data/sub-02_task-forgot.fsaverage6.lh.tproject.gii',
-                            'sub-15': 'data/sub-15_task-forgot.fsaverage6.lh.tproject.gii'},
-                   'data_trims': [8, 8]}}
+metadata = {'black':
+             {'stimulus_duration': 837,
+              'n_TRs': 574,
+              'timestamps': 'transcripts/black_timestamps.txt',
+              'model': 'transcripts/black_word2vec.npy',
+              'model_trims': [0, 0],
+              'data':
+                {'sub-02':
+                  'data/sub-02_task-black.fsaverage6.lh.tproject.gii',
+                 'sub-15':
+                  'data/sub-15_task-black.fsaverage6.lh.tproject.gii'},
+              'data_trims': [8, 8]},
+            'forgot':
+              {'stimulus_duration': 800,
+               'n_TRs': 550,
+               'timestamps': 'transcripts/forgot_timestamps.txt',
+               'model': 'transcripts/forgot_word2vec.npy',
+               'model_trims': [0, 0],
+               'data':
+                 {'sub-02':
+                   'data/sub-02_task-forgot.fsaverage6.lh.tproject.gii',
+                  'sub-15':
+                   'data/sub-15_task-forgot.fsaverage6.lh.tproject.gii'},
+               'data_trims': [8, 8]}}
 
-with open('input_fns.json', 'w') as f:
-    json.dump(input_fns, f, sort_keys=True, indent=2)
+with open('metadata.json', 'w') as f:
+    json.dump(metadata, f, sort_keys=True, indent=2)
