c6a32fa17e855f2d507ce1837f0fc606f46d475c
Sam Nastase <sam.nastase@gmail.com>
2019-05-26 18:21:42 -0400

    Support for aggregating multiple subjects

--- encoding_model.py
+++ encoding_model.py
@@ -3,7 +3,8 @@ import numpy as np
 from sklearn.linear_model import Ridge
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV, KFold
-from scipy.stats import zscore
+from scipy.stats import rankdata, zscore
+from scipy.spatial.distance import cdist
 from utils import array_correlation, read_gifti, write_gifti
 
 # Load dictionary of input filenames
@@ -15,8 +16,19 @@ n_vertices = 40962
 model_ndim = 300
 delays = [2, 3, 4, 5]
 
+rois = ['EAC', 'AAC', 'TPOJ', 'PCC']
+roi = 'PCC'
+hemi = 'lh'
+
+stories = ['black', 'forgot']
+exclude = [7, 11, 12, 13, 26, 27]
+subject_list = [f'sub-{i+1:02}' for i in range(48)
+            if i not in exclude]
+subject_list = ['sub-01', 'sub-02', 'sub-10', 'sub-15']
+subjects = {story: subject_list for story in stories}
+
 # Load mask in fsaverage6 space
-mask = np.load('data/cortical_mask_lh.npy')
+mask = np.load(f'data/{roi}_mask_{hemi}.npy').astype(bool)
 
 
 # Function for delaying embedding by several TRs
@@ -43,13 +55,15 @@ def delay_embedding(embedding, delays=[2, 3, 4, 5],
 
 
 # Load surface data and trim
-def load_inputs(metadata, stories=None, mask=None):
+def load_inputs(metadata, subjects=None,
+                stories=None, hemi=None, mask=None):
     
     if not stories:
         stories = metadata.keys()
     
     inputs = {}
     for story in stories:
+
         inputs[story] = {}
         
         # Load, trim, and delay embedding
@@ -60,28 +74,35 @@ def load_inputs(metadata, stories=None, mask=None):
         inputs[story]['model'] = model
         print(f'Loaded model for story "{story}"')
         
-        # Get data for all subejcts
+        # Get data for all or some subjects
         inputs[story]['data'] = {}
-        subjects = metadata[story]['data'].keys()
+        
+        if not subjects:
+            subject_list = metadata[story]['data'].keys()
+        else:
+            subject_list = subjects[story]
+
         n_subjects = 0
-        for subject in subjects:
-            data_fn = metadata[story]['data'][subject]
-            surf_data = read_gifti(data_fn)
+        for subject in subject_list:
             
+            data_fn = metadata[story]['data'][subject][hemi]
+            surf_data = read_gifti(data_fn)
+
             # Trim data
             data_trims = metadata[story]['data_trims']
-            surf_data = surf_data[data_trims[0]:(-data_trims[1] or None), :]
-            
+            surf_data = surf_data[data_trims[0]:(
+                -data_trims[1] or None), :]
+
             # Check that model and data have matching length
             if not model.shape[0] == surf_data.shape[0]:
                 raise ValueError(f"Model shape {model.shape} does not "
                                  f"match data shape {surf_data.shape} "
                                  f'for story "{story}"')
-            
+
             # Optionally apply mask
             if isinstance(mask, np.ndarray):
                 surf_data = surf_data[:, mask]
-            
+
             inputs[story]['data'][subject] = surf_data
             
             n_subjects +=1
@@ -90,11 +111,12 @@ def load_inputs(metadata, stories=None, mask=None):
 
     return inputs
 
-inputs = load_inputs(metadata, stories=['black', 'forgot'], mask=mask)
+inputs = load_inputs(metadata, subjects=subjects,
+                     stories=stories, hemi=hemi, mask=mask)
 
 
 # Split into train and test
-def split_half(inputs, zscore_data=True, zscore_model=False):
+def story_split(inputs, zscore_data=True, zscore_model=False):
     
     model_splits, data_splits = {}, {}
     for story in inputs:
@@ -135,18 +157,68 @@ def split_half(inputs, zscore_data=True, zscore_model=False):
     return model_splits, data_splits
 
 
-model_splits, data_splits = split_half(inputs)
+model_splits, data_splits = story_split(inputs)
+
+
+# Function for selecting and aggregating subjects
+def aggregate_subjects(data, subject_list, aggregation='average',
+                       cv_fold=0, train_test=0):
+    
+    # Allow for easy single subject input
+    if type(subject_list) == str:
+        subject = subject_list
+        data = data[subject][cv_fold][train_test]
+        
+    else:
+        if len(subject_list) == 1:
+            data = data[subject_list[0]][cv_fold][train_test]
+        else:
+            n_subjects = len(subject_list)
+            assert n_subjects > 1
+
+            # Check subjects are in the data
+            for subject in subject_list:
+                assert subject in data.keys()
+
+            # Compile test subjects
+            data_list = []
+            for subject in subject_list:
+                data_list.append(data[subject][cv_fold][train_test])
+
+            # Average time data across subjects
+            if aggregation == 'average' and n_subjects > 1:
+                data = np.mean(data_list, axis=0)
+
+            elif aggregation == 'concatenate' and n_subjects > 1:
+                data = np.vstack(data_list)
+    
+    return data
 
 train_story, test_story = 'forgot', 'forgot'
-train_subject, test_subject = 'sub-15', 'sub-02'
-cv_fold = 1
+train_subjects = ['sub-01', 'sub-02', 'sub-10']
+test_subjects = ['sub-15']
+cv_fold = 0
+aggregation = 'average'
 
-train_model = model_splits[train_story][cv_fold][0]
-test_model = model_splits[test_story][cv_fold][1]
+if aggregation == 'average':
+    train_model = model_splits[train_story][cv_fold][0]
+    test_model = model_splits[test_story][cv_fold][1]
+elif aggregation == 'concatenation':
+    train_model = np.tile(model_splits[train_story][cv_fold][0],
+                          (len(train_subjects), 1))
+    test_model = np.tile(model_splits[test_story][cv_fold][1],
+                         (len(test_subjects), 1))
 
-train_data = data_splits[train_story][train_subject][cv_fold][0]
-test_data = data_splits[test_story][test_subject][cv_fold][1]
-
+train_data = aggregate_subjects(data_splits[train_story],
+                                train_subjects,
+                                aggregation=aggregation,
+                                cv_fold=cv_fold,
+                                train_test=0)
+                                
+test_data = aggregate_subjects(data_splits[test_story],
+                               test_subjects,
+                               cv_fold=cv_fold,
+                               train_test=1)
 
 # Make custom correlation scorer
 correlation_scorer = make_scorer(array_correlation)
@@ -202,6 +274,9 @@ def rank_accuracy(predicted_model, test_model, mean=True):
     return ranks
 
 accuracy = rank_accuracy(predicted_model, collapse_test_model)
+
+print(np.mean(performance), np.amax(performance))
+print(accuracy)
 
 
 # Fill results back in cortical mask and write GIfTI
